71e1667af5a4ca2138295d0f0b49f1d93e1d5560
unknown <thejasondutys@hotmail.com>
2022-08-04 22:13:09 -0400

    Se ha hecho un cambio

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 3800,
+        disableOnInteraction: false,
+      },
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+  });
